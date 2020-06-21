cc781dc49174f37675a1060159b53b34b40e7ce9
Junjie Chen <junjie.chen18@gmail.com>
2020-06-21 16:22:21 -0400

    feat(commentSchema): define user document

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({ content: String });
+const commentSchema = new Schema({
+  content: String,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
