6efd4f8e34e7702be3b2bf5e44b8353e0baea3af
Junjie Chen <junjie.chen18@gmail.com>
2020-06-21 23:09:27 -0400

    feat(middleware): delete associated blog posts before deleting a user

--- test/middleware.js
+++ test/middleware.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai');
 const User = require('../src/models/User');
 const BlogPost = require('../src/models/BlogPost');
 
@@ -19,5 +20,15 @@ describe('Middleware', () => {
       blogPost.save()
     ])
       .then(() => done());
+  });
+
+  it('should delete associated blog posts before deleting a user', done => {
+    user.remove()
+      .then(() => BlogPost.estimatedDocumentCount())
+      .then(count => {
+        expect(count).to.equal(0);
+
+        done();
+      });
   });
 });
